ee79ba5bde6405fd205767bde5d50553fbbc43e3
Manasseh Ankrah <solomonankrah24@gmail.com>
2024-06-01 00:20:27 +0000

    Modified types.d.ts

--- src/app/types/types.d.ts
+++ src/app/types/types.d.ts
@@ -1,18 +1,21 @@
 import React from "react";
 
-export type nextPagesTypes = { page: number; totalPages: number };
+export type nextPagesTypes = {
+  page: number;
+  totalPages: number;
+};
 
-export interface stateProp {
+export type stateProp = {
   isDarkMode: boolean;
-}
+};
 
-export interface actionProp {
+export type actionProp = {
   type: string;
   item: stateProp;
-}
+};
 
-export interface providerProps {
+export type providerProps = {
   reducer: React.Reducer<stateProp, actionProp>;
   initialState: stateProp;
   children: React.ReactNode;
-}
+};
