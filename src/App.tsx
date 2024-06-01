e7ae2313c108ffabeb8300c4e0a6eb67140f4242
Manasseh Ankrah <solomonankrah24@gmail.com>
2024-06-01 16:42:36 +0000

    Modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./app/router";
-import { ThemeProvider } from "./app/context/ThemeContext";
-import reducer, { initialState } from "./app/context/reducer";
 
 function App() {
-  return (
-    // <ThemeProvider initialState={initialState} reducer={reducer}>
-    <RouterProvider router={router} />
-    // </ThemeProvider>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/app/context/ThemeContext.tsx
+++ src/app/context/ThemeContext.tsx
@@ -1,22 +1,4 @@
-// import React, { createContext, useContext, useReducer } from "react";
-// import { providerProps } from "../types/types";
-
-// export const ThemeContext = createContext<any>(undefined);
-
-// export const ThemeProvider = ({
-//   reducer,
-//   initialState,
-//   children,
-// }: providerProps) => {
-//   const value = useReducer(reducer, initialState);
-//   return (
-//     <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
-//   );
-// };
-
-// export const useStateValue = () => useContext(ThemeContext);
-
-import React, { createContext, useContext, useReducer, ReactNode } from "react";
+import React, { createContext, useContext, useReducer } from "react";
 import { providerProps, stateProp, actionProp } from "../types/types";
 
 export const ThemeContext = createContext<
